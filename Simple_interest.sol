5395b157306de0dff09c4be8b6f2a560c79fbcb0
arunkotla <arunkkotla1998@gmail.com>
2023-08-18 13:25:26 +0530

    Adding SimpleInterest calculating function

--- Simple_interest.sol
+++ Simple_interest.sol
@@ -3,13 +3,39 @@ pragma solidity ^0.8.0;
 
 contract Simple_Interest{
 
-       string[]  name;
+       struct DepositInfo{
+           string name;
+           address account;
+           uint amount;
+           uint timestamp;           
+       }
+
+       DepositInfo[] public deposits;
 
     address payable public owner = payable(msg.sender);
 
     function Deposit(string memory _name) payable public {
         require(msg.value > 0, "amount should be more than 0");
         owner.transfer(msg.value);
-        name.push(_name);
+        deposits.push(
+            DepositInfo(
+                {
+                name : _name,
+                account : msg.sender,
+                amount : msg.value,
+                timestamp : block.timestamp
+                }
+            )
+        );
     }
+
+    function calculateInterest(uint depositIndex, uint interestRate, uint timePeriod) public view returns (uint){
+        require(depositIndex < deposits.length, "Invalid deposit index");
+        uint principle =  deposits[depositIndex].amount;
+        uint interest = (principle*interestRate*timePeriod)/100;
+
+        return interest;
+
+    }
+
 }
